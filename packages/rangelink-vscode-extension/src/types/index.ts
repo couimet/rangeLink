80d3954e6ce723cb8d62d34fbf0a983654795ce1
Charles Ouimet <charles.ouimet@shopify.com>
2025-11-08 09:35:06 -0500

    Cleaned up non-existing `export`

--- packages/rangelink-vscode-extension/src/types/index.ts
+++ packages/rangelink-vscode-extension/src/types/index.ts
@@ -1,3 +1,2 @@
 export type { RangeLinkClickArgs } from './RangeLinkClickArgs';
 export type { RangeLinkTerminalLink } from './RangeLinkTerminalLink';
-export type { WithRequired } from './WithRequired';
