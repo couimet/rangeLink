073be88be85cd804f910040de45cb54ae9ab2ee7
Charles Ouimet <charles.ouimet@shopify.com>
2025-12-11 22:38:04 -0500

    Removed now-deleted `export`s

--- packages/rangelink-vscode-extension/src/destinations/index.ts
+++ packages/rangelink-vscode-extension/src/destinations/index.ts
@@ -6,10 +6,3 @@ export * from './ComposablePasteDestination';
 export * from './DestinationRegistry';
 export * from './destinationBuilders';
 export * from './PasteDestinationManager';
-
-export * from './ChatAssistantDestination';
-export * from './ChatPasteHelper';
-export * from './ChatPasteHelperFactory';
-export * from './ClaudeCodeDestination';
-export * from './CursorAIDestination';
-export * from './GitHubCopilotChatDestination';
