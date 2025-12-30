b12d917d50a43b788655d63eb5bed74be7bcc492
Charles Ouimet <charles.ouimet@shopify.com>
2025-12-30 17:43:34 -0500

    Improve wording to make it clearer

--- packages/rangelink-vscode-extension/src/i18n/messages.en.ts
+++ packages/rangelink-vscode-extension/src/i18n/messages.en.ts
@@ -79,7 +79,7 @@ export const messagesEn: Record<MessageCode, string> = {
   [MessageCode.STATUS_BAR_JUMP_SUCCESS_TERMINAL]: '✓ Focused Terminal: "{resourceName}"',
   [MessageCode.STATUS_BAR_BOOKMARK_SAVED]: '✓ Bookmark saved: {label}',
   [MessageCode.STATUS_BAR_LINK_COPIED_TO_CLIPBOARD]: '✓ {linkTypeName} copied to clipboard',
-  [MessageCode.STATUS_BAR_MENU_BOOKMARKS_ADD_CURRENT]: '$(add) Add Current Selection',
+  [MessageCode.STATUS_BAR_MENU_BOOKMARKS_ADD_CURRENT]: '$(add) Save Selection as Bookmark',
   [MessageCode.STATUS_BAR_MENU_BOOKMARKS_EMPTY]: 'No bookmarks yet',
   [MessageCode.STATUS_BAR_MENU_BOOKMARKS_MANAGE]: '$(gear) Manage Bookmarks...',
   [MessageCode.STATUS_BAR_MENU_BOOKMARKS_SECTION_LABEL]: 'Bookmarks',
