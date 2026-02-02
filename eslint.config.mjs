dda9ff42ae62ea3b4d96232c2468fc469a0675c5
Charles Ouimet <charles.ouimet@shopify.com>
2026-02-01 23:12:56 -0500

    Add missing `ignores` for `.history`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ export default [
       '**/out/**',
       '**/node_modules/**',
       '**/coverage/**',
+      '**/.history/**',
       '**/*.vsix',
       '**/*.config.js',
       '**/*.config.mjs',
